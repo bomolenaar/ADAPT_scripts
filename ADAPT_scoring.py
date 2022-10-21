c962c38d37b87dd11aea96086183129da9a5d3bb
Bo Molenaar <bo.molenaar@ru.nl>
2022-10-21 12:38:35 +0200

    changed ASR adapt processing to optional, introduced adapt descriptive stats

--- ADAPT_scoring.py
+++ ADAPT_scoring.py
@@ -12,13 +12,14 @@ This script takes a set of ADAPT output spreadsheets: 1 baseline Prompt-MT and a
 and calculates the cohen kappa score for agreement between the baseline and each ASR output.
 """
 
-if len(sys.argv) < 4:
-    raise ValueError("Please call this script with ADAPT_scoring.py [ADAPT baseline] [error words output] [error sentences output]")
+if len(sys.argv) not in {2, 3, 4}:
+    raise ValueError("Please call this script with ADAPT_scoring.py [ADAPT baseline] [optional error words output] [optional error sentences output]")
 
 # ADAPT baseline file path
 adapt_baseline_path = sys.argv[1]
-words_out = sys.argv[2]
-sents_out = sys.argv[3]
+if len(sys.argv) == 4:
+    words_out = sys.argv[2]
+    sents_out = sys.argv[3]
 
 
 def read_adapt_table(adapt_path):
@@ -55,33 +56,48 @@ def read_adapt_table(adapt_path):
     return df2
 
 
-def extract_words_sents(df, error_words_excel_path, error_sents_excel_path):
+def extract_words_sents(df, error_words_excel_path=None, error_sents_excel_path=None):
     # now extract only words marked as incorrect by ADAPT to investigate
     incorrect_items = df.loc[df["correct"] == 0]
-    # write list to excel
-    incorrect_items.to_excel(error_words_excel_path)
     print("Incorrect items:", len(incorrect_items))
 
     # print the % of words marked as incorrect
     deg_incor_w = len(incorrect_items)/len(df)
     print("Degree incorrect items:", deg_incor_w, '\n')
 
+    # print the mean and sd for ADAPT distance score per word
+    adapt_mean_incor_w = incorrect_items['dist_score'].mean()
+    adapt_sd_incor_w = incorrect_items['dist_score'].std()
+    print("Mean ADAPT distance score for incorrect items:", adapt_mean_incor_w)
+    print("SD of ADAPT distance score for incorrect items:", adapt_sd_incor_w, '\n')
+
     # make a list of all sentences with at least one word marked as incorrect (present in incorrect words list)
     incorrect_list = sorted(list({wav_id for wav_id, row in incorrect_items.iterrows()}))
     incorrect_sents = pd.DataFrame(df.loc[incorrect_list])
     n_incorrect_sents = set(incorrect_sents.index)
-    # write list to excel
-    incorrect_sents.to_excel(error_sents_excel_path)
     print("Incorrect sentences:", len(n_incorrect_sents))
 
     # print the % of sentences marked as incorrect
     deg_incor_s = len(incorrect_sents.index.unique())/len(df.index.unique())
     print("Degree incorrect sentences:", deg_incor_s, '\n')
 
+    # print the mean and sd for ADAPT distance score per sentence
+    adapt_mean_incor_s = incorrect_sents.groupby('wav_id')['dist_score'].sum().mean()
+    adapt_sd_incor_s = incorrect_sents.groupby('wav_id')['dist_score'].sum().std()
+    print("Mean ADAPT distance score for incorrect sentences:", adapt_mean_incor_s)
+    print("SD of ADAPT distance score for incorrect sentences:", adapt_sd_incor_s, '\n')
+
+    if error_words_excel_path:
+        # write list to excel
+        incorrect_items.to_excel(error_words_excel_path)
+    if error_sents_excel_path:
+        # write list to excel
+        incorrect_sents.to_excel(error_sents_excel_path)
+
 
 print("Dimensions for baseline ADAPT file:")
 ADAPT_base = read_adapt_table(adapt_baseline_path)
-extract_words_sents(ADAPT_base, words_out, sents_out)
+extract_words_sents(ADAPT_base)
 
 # for sheetnr in range(0, len(sys.argv) - 3):
 #     sheet = sys.argv[3 + sheetnr]
